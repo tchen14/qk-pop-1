99efd596d9e01e89df2cd5753577893a2ba5a1d2
Jordan Emmanuel <fat_flying_pigs@yahoo.com>
2014-10-07 18:00:26 -0700

    Fixed layout. No functional changes

--- Assets/Scripts/Log.cs
+++ Assets/Scripts/Log.cs
@@ -9,152 +9,152 @@ using UnityEditor;
 [SerializeField]
 public class Log : EditorWindow
 {
-		[SerializeField]
-		public static List<string>
-				logStrings = new List<string> ();
+	[SerializeField]
+	public static List<string>
+		logStrings = new List<string>();
 	
-		//[SerializeField]
-		public List<bool>
-				logBools = new List<bool> ();
+	//[SerializeField]
+	public List<bool>
+		logBools = new List<bool>();
 	
-		[SerializeField]
-		public static bool
-				enabled = true, all = true;
+	[SerializeField]
+	public static bool
+		enabled = true, all = true;
 	
 	#if UNITY_EDITOR
-		// Add menu named "My Window" to the Window menu
-		[MenuItem ("Window/Log Manager")]
-		static void Init ()
-		{
-				EditorWindow.GetWindow (typeof(Log));
-		}
+	// Add menu named "My Window" to the Window menu
+	[MenuItem ("Window/Debug Log Manager")]
+	static void Init()
+	{
+		EditorWindow.GetWindow(typeof(Log)).title = "Debug Log Messages";
+	}
 	
-		//this function is called any time the play button is pressed, after the OnDisable function
-		void OnEnable ()
-		{
-				if (logStrings.Count == 0) {
-						string text = System.IO.File.ReadAllText (Application.dataPath + "/Editor/logKeys.json");
-						var N = JSON.Parse (text);
+	//this function is called any time the play button is pressed, after the OnDisable function
+	void OnEnable()
+	{
+		if (logStrings.Count == 0) {
+			string text = System.IO.File.ReadAllText(Application.dataPath + "/Editor/logKeys.json");
+			var N = JSON.Parse(text);
 			
-						logStrings.Clear ();
-						for (int i = 0; i < N.Count; i++) {
-								logStrings.Add (N [i].Value);
-						}
-				}
+			logStrings.Clear();
+			for (int i = 0; i < N.Count; i++) {
+				logStrings.Add(N [i].Value);
+			}
+		}
 		
-				logBools.Clear ();
-				for (int i = 0; i < logStrings.Count; i++) {
-						logBools.Add (EditorPrefs.GetBool (logStrings [i]));
-				}
+		logBools.Clear();
+		for (int i = 0; i < logStrings.Count; i++) {
+			logBools.Add(EditorPrefs.GetBool(logStrings [i]));
 		}
+	}
 	
-		//this function is called any time the play button is pressed, before the OnEnable function
-		void OnDisable ()
-		{
-				for (int i = 0; i < logStrings.Count; i++) {
-						EditorPrefs.SetBool (logStrings [i], logBools [i]);
-				}
+	//this function is called any time the play button is pressed, before the OnEnable function
+	void OnDisable()
+	{
+		for (int i = 0; i < logStrings.Count; i++) {
+			EditorPrefs.SetBool(logStrings [i], logBools [i]);
 		}
+	}
 	
-		//this function is called anytime the window is closed
-		void OnDestroy ()
-		{
-				for (int i = 0; i < logStrings.Count; i++) {
-						EditorPrefs.SetBool (logStrings [i], logBools [i]);
-				}
+	//this function is called anytime the window is closed
+	void OnDestroy()
+	{
+		for (int i = 0; i < logStrings.Count; i++) {
+			EditorPrefs.SetBool(logStrings [i], logBools [i]);
 		}
+	}
 	
-		void OnGUI ()
-		{
-				EditorGUILayout.BeginHorizontal ();
-				GUILayout.Label ("Debug Log Messages", EditorStyles.boldLabel);
-				enabled = EditorGUILayout.Toggle ("", enabled);
-				if (enabled)
-						GUILayout.Label ("is Enabled", EditorStyles.boldLabel);
-				else
-						GUILayout.Label ("is Disabled", EditorStyles.boldLabel);
-				EditorGUILayout.EndHorizontal ();
-				EditorGUILayout.Space ();
+	void OnGUI()
+	{
+		EditorGUILayout.Space();
+		if (enabled) {
+			enabled = EditorGUILayout.ToggleLeft("Logs are Enabled", enabled, EditorStyles.boldLabel);
+		} else {
+			enabled = EditorGUILayout.ToggleLeft("Logs are Disabled", enabled, EditorStyles.boldLabel);
+		}
+		EditorGUILayout.Space();
 		
-				if (enabled) {
-						if (all)
-								all = EditorGUILayout.BeginToggleGroup ("Showing Selected", all);
-						else
-								all = EditorGUILayout.BeginToggleGroup ("Showing All Logs", all);
+		if (enabled) {
+			if (all) {
+				all = EditorGUILayout.BeginToggleGroup("Showing Selected Logs", all);
+			} else {
+				all = EditorGUILayout.BeginToggleGroup("Showing All Logs", all);
+			}
+			EditorGUILayout.Space();
 			
-						if (logStrings.Count == 0) {
-								string text = System.IO.File.ReadAllText (Application.dataPath + "/Editor/logKeys.json");
-								var N = JSON.Parse (text);
+			if (logStrings.Count == 0) {
+				string text = System.IO.File.ReadAllText(Application.dataPath + "/Editor/logKeys.json");
+				var N = JSON.Parse(text);
 				
-								logStrings.Clear ();
-								logBools.Clear ();
-								for (int i = 0; i < N.Count; i++) {
-										logStrings.Add (N [i].Value);
-										logBools.Add (false);
-								}
-						}
-			
-						for (int i = 0; i < logBools.Count; i++) {
-								logBools [i] = EditorGUILayout.Toggle (logStrings [i], logBools [i]);
-						}
-						EditorGUILayout.EndToggleGroup ();
+				logStrings.Clear();
+				logBools.Clear();
+				for (int i = 0; i < N.Count; i++) {
+					logStrings.Add(N [i].Value);
+					logBools.Add(false);
 				}
-		
-				//enabled = EditorGUILayout.BeginToggleGroup ("Enable Debug Logs", enabled);
-				//EditorGUILayout.EndToggleGroup ();
-				//myString = EditorGUILayout.TextField ("Text Field", myString);
-				//myFloat = EditorGUILayout.Slider ("Slider", myFloat, -3, 3);
+			}
+			
+			for (int i = 0; i < logBools.Count; i++) {
+				logBools [i] = EditorGUILayout.Toggle(logStrings [i], logBools [i]);
+			}
+			EditorGUILayout.EndToggleGroup();
 		}
+		
+		//enabled = EditorGUILayout.BeginToggleGroup ("Enable Debug Logs", enabled);
+		//EditorGUILayout.EndToggleGroup ();
+		//myString = EditorGUILayout.TextField ("Text Field", myString);
+		//myFloat = EditorGUILayout.Slider ("Slider", myFloat, -3, 3);
+	}
 	#endif
 	
-		public static void M (string key, string message)
-		{
-				Message (key, message);
-		}
+	public static void M(string key, string message)
+	{
+		Message(key, message);
+	}
 	
-		public static void W (string key, string message)
-		{
-				Warning (key, message);
-		}
+	public static void W(string key, string message)
+	{
+		Warning(key, message);
+	}
 	
-		public static void E (string key, string message)
-		{
-				Error (key, message);
-		}
+	public static void E(string key, string message)
+	{
+		Error(key, message);
+	}
 	
 	#if UNITY_EDITOR
-		public static void Message (string key, string message)
-		{
-				if (!enabled) {
-						return;
-				}
-		
-				if ((!all) || (logStrings.Contains (key) && EditorPrefs.GetBool (key))) {
-						Debug.Log (message);
-				}
+	public static void Message(string key, string message)
+	{
+		if (!enabled) {
+			return;
 		}
+		
+		if ((!all) || (logStrings.Contains(key) && EditorPrefs.GetBool(key))) {
+			Debug.Log(message);
+		}
+	}
 	
-		public static void Warning (string key, string message)
-		{
-				if (!enabled) {
-						return;
-				}
-		
-				if ((!all) || (logStrings.Contains (key) && EditorPrefs.GetBool (key))) {
-						Debug.LogWarning (message);
-				}
+	public static void Warning(string key, string message)
+	{
+		if (!enabled) {
+			return;
 		}
+		
+		if ((!all) || (logStrings.Contains(key) && EditorPrefs.GetBool(key))) {
+			Debug.LogWarning(message);
+		}
+	}
 	
-		public static void Error (string key, string message)
-		{
-				if (!enabled) {
-						return;
-				}
-		
-				if ((!all) || (logStrings.Contains (key) && EditorPrefs.GetBool (key))) {
-						Debug.LogError (message);
-				}
+	public static void Error(string key, string message)
+	{
+		if (!enabled) {
+			return;
 		}
+		
+		if ((!all) || (logStrings.Contains(key) && EditorPrefs.GetBool(key))) {
+			Debug.LogError(message);
+		}
+	}
 	#else
 	public static void Message(string key, string message){
 		;
