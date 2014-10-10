7c376174097298a45f3c84361cd48cb6b6477b60
Ryan Evans <cagd495@mailinator.com>
2014-10-09 19:17:26 -0700

    save progress commit. progress on event manager and serialization

---
+++ Assets/Scripts/PatriotsOfThePast/Checkpoint.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour
+{
+	public string id = "";	//this variable is for use in the editor.
+	public float minDist = 0.0f; //this is the minimum distance the CheckpointTrigger needs to use the Checkpoint
+}
---
+++ Assets/Scripts/PatriotsOfThePast/CheckpointTrigger.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointTrigger : MonoBehaviour
+{
+	public float checkUpdateSpeed = 1.0f;
+	
+	void Start()
+	{
+		StartCoroutine("SlowUpdate");
+	}
+	
+	IEnumerator SlowUpdate()
+	{
+	
+		yield return new WaitForSeconds(checkUpdateSpeed);
+		StartCoroutine("SlowUpdate");
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.GetComponent<Checkpoint>()) {
+			//run short term checkpoint saving code here
+		}
+	}
+
+	private void QuickAutoSave(){
+
+	}
+
+	private void QuestAutoSave(){
+		
+	}
+
+	//testing code only
+	#if UNITY_EDITOR
+	private void ManualSave(){
+		
+	}
+	#endif
+}
