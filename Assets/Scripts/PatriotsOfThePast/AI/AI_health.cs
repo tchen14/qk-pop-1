d6d03a52444fe35a55a1095e3efceaaf411556ea
Jared DuBose <cagd495@mailinator.com>
2014-10-09 19:47:04 -0700

    Complete Structure for the AI Still in temperamental stages but is functioning in its base cases Should not be implemented by level design

---
+++ Assets/Scripts/PatriotsOfThePast/AI/AI_health.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI_health : MonoBehaviour {
+
+	public int hurt(int damage)
+	{
+		int HP = GetComponent<AI_main> ().AI_HP;
+		HP -= damage;
+		return HP;
+
+	}
+	public int heal(int health)
+	{
+		int HP = GetComponent<AI_main> ().AI_HP;
+		HP += health;
+		return HP;
+	}
+}
