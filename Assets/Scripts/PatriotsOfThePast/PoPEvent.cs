c37fa0c7718020651b17009271b395ff16f55f98
Jordan Emmanuel <cagd495@mailinator.com>
2014-10-14 19:39:36 -0700

    Adding files for quests and checkpoints

---
+++ Assets/Scripts/PatriotsOfThePast/PoPEvent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoPEvent : Placeholder
+{
+	[SerializeField]
+	private string prerequisite = ""; //this string is for to check for a completed pre-requisite quest
+
+	//other prerequsite (eg: item count, time, %map explored, etc.) need a system to be made
+	[SerializeField]
+	private string quest = ""; //if this event will start a quest
+	[SerializeField]
+	private Vector3 cameraPos, lookAtPos; //camera position for event
+
+	public void BeginEvent(){
+		//camera movements
+		/*
+		 * Camera movements need to have a line segment (two nodes) that dictate where the camera (position) and where it looks at (rotation)
+		 * Further effects would be to give the camera simple movement functionality (GoKit)
+		 */
+
+		//Prep dialogue code, text + input (next/skip)
+		//at the end of the dialogue, activate quest (in dailogue function)
+	}
+}
