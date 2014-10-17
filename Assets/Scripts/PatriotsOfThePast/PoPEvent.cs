140647bd2a6b31c48ba6ec9920f1dd4196e24109
Jordan Emmanuel <cagd495@mailinator.com>
2014-10-16 19:50:28 -0700

    Checkpoint and event gizmos

--- Assets/Scripts/PatriotsOfThePast/PoPEvent.cs
+++ Assets/Scripts/PatriotsOfThePast/PoPEvent.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(Collider))]
 public class PoPEvent : Placeholder
 {
 	[SerializeField]
@@ -21,5 +22,14 @@ public class PoPEvent : Placeholder
 
 		//Prep dialogue code, text + input (next/skip)
 		//at the end of the dialogue, activate quest (in dailogue function)
+	}
+
+	void OnDrawGizmos()
+	{
+		//Gizmos.DrawIcon(transform.position, "eventGizmo.png");
+
+		// Draw a yellow sphere at the transform's position
+		Gizmos.color = Color.yellow / 3;
+		Gizmos.DrawSphere (transform.position, this.GetComponent<SphereCollider>().radius);
 	}
 }
