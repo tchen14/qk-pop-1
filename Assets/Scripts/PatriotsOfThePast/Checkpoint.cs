b4cd240c2e7ab26973cb3386069c7e387bae7eed
Jordan Emmanuel <cagd495@mailinator.com>
2014-10-16 19:50:28 -0700

    Checkpoint and event gizmos

--- Assets/Scripts/PatriotsOfThePast/Checkpoint.cs
+++ Assets/Scripts/PatriotsOfThePast/Checkpoint.cs
@@ -5,4 +5,9 @@ using System.Collections;
 public class Checkpoint : Placeholder
 {
 	public float minDist = 0.0f; //this is the minimum distance away from the Checkpoint that CheckpointTrigger needs to be
+		
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawIcon(transform.position, "checkpointGizmo.png");
+	}
 }
