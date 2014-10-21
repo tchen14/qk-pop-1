f2e2b0d4d62a5bb6a196c4161349b45f87a4605e
Jordan Emmanuel <fat_flying_pigs@yahoo.com>
2014-10-21 00:12:22 -0700

    removed testing code

--- Assets/Scripts/SceneManager/PoPSceneManager.cs
+++ Assets/Scripts/SceneManager/PoPSceneManager.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class PoPSceneManager : SceneManager
 {
 
-	void Start()
-	{
-		//PoPEventManager instance = new PoPEventManager();
-	}
+		void Start ()
+		{
+				//PoPEventManager instance = new PoPEventManager();
+		}
 
-	//testing code until we get Scaleform UI fully working
-	void OnGUI () {
+		//testing code until we get Scaleform UI fully working
+		/*void OnGUI () {
 		// Make a background box
 		GUI.Box(new Rect(10,10,100,90), "Testing Menu");
 		
@@ -18,16 +18,16 @@ public class PoPSceneManager : SceneManager
 		if(GUI.Button(new Rect(20,40,80,20), "Load Menu")) {
 			StartMenuScene();
 		}
-	}
+	}*/
 
-	void Update()
-	{
+		void Update ()
+		{
 	
-	}
+		}
 	
-	void StartMenuScene()
-	{
-		MasterManager.Instance.ToggleLevel("Menu");
-		MasterManager.Instance.LoadLevels();
-	}
+		void StartMenuScene ()
+		{
+				MasterManager.Instance.ToggleLevel ("Menu");
+				MasterManager.Instance.LoadLevels ();
+		}
 }
