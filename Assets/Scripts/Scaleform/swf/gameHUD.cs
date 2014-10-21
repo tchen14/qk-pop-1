8a37f4f87899b68b4b1fd7bde199ea72f5371006
Jordan Emmanuel <fat_flying_pigs@yahoo.com>
2014-10-20 21:48:36 -0700

    fixing previous core commit as well as adding UI scripts

---
+++ Assets/Scripts/Scaleform/swf/gameHUD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Scaleform;
+
+
+
+public class gameHUD : Movie
+{
+	protected Value	swfMovie = null;
+	
+	
+	//private SWFCamera parent = null;
+	
+
+	public gameHUD(SWFCamera parent, SFManager sfmgr, SFMovieCreationParams cp) :
+	base(sfmgr, cp){
+		//this.parent = parent;
+		SFMgr = sfmgr;
+		this.SetFocus(true);
+	}
+	
+	public void OnRegisterSWFCallback(Value swfRef){
+		swfMovie = swfRef;
+		
+		
+		Debug.Log ("SWF Callback!");
+	}
+	
+
+	
+}	
