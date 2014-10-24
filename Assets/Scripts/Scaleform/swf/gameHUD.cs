c5d007da3cbdb3dcdfcf31aae1e7b89f1eb4ef97
Anthony Graceffa <cagd495@mailinator.com>
2014-10-23 19:52:06 -0700

    button fx

--- Assets/Scripts/Scaleform/swf/gameHUD.cs
+++ Assets/Scripts/Scaleform/swf/gameHUD.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*---------------------------------*/
+/*do not access these public functions to change HUD Info, call the ones in HUDCam instead */
+/*---------------------------------*/
+
+using System;
 using System.Collections;
 using UnityEngine;
 using Scaleform;
@@ -7,11 +11,11 @@ using Scaleform;
 
 public class gameHUD : Movie
 {
+
 	protected Value	swfMovie = null;
-	public float health;
-	
 	//private SWFCamera parent = null;
-	
+	public bool liveBattTest = true;
+
 
 	public gameHUD(HUDCam parent, SFManager sfmgr, SFMovieCreationParams cp) :
 	base(sfmgr, cp){
@@ -25,14 +29,35 @@ public class gameHUD : Movie
 		//Debug.Log ("SWF Callback!");
 	}
 
-	public void updateBattery () {
+	//used for testing all updates with default values
+	public void testUpdates () {
+		updateObjective ("Goal: Give Anthony 20 dollars");
+		updateBattery (78f);
+	}
+
+	//call this to update battery display
+	public void updateBattery (float battLevel) {
 		Value bBar = swfMovie.GetMember ("bBar");
 		if (bBar != null) {
 			SFDisplayInfo dInfo = bBar.GetDisplayInfo();
 
 			if(dInfo == null) return;
-			dInfo.XScale = health;
+
+			//this sets the scale on the x axis(the width) of the battery bar,the max width is 78, adjust math acoordingly
+			dInfo.XScale = (double)battLevel;
+			bBar.SetDisplayInfo(dInfo);
 
 		}
 	}
+
+	//call this to update objective display text at top of the HUD, the passed string becomes the new objective
+	public void updateObjective (string objective) {
+		swfMovie.Invoke ("updateObjective", objective);
+	}
+
+	public void chargeGraphic () {
+		swfMovie.Invoke ("showChargeGraphic");
+	}
+
+
 }	
