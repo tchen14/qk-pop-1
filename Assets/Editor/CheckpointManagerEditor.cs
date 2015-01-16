73684440f57a4fce53b7966fcfedf067ba29a450
Jordan Emmanuel <fat_flying_pigs@yahoo.com>
2015-01-16 04:33:55 -0800

    Majority checkpoint push. All features working except loading data from a file

--- Assets/Editor/CheckpointManagerEditor.cs
+++ Assets/Editor/CheckpointManagerEditor.cs
@@ -3,95 +3,124 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 
-[CustomEditor (typeof (CheckpointManager))]
-public class CheckpointManagerEditor : Editor {
+[CustomEditor (typeof(CheckpointManager))]
+public class CheckpointManagerEditor : Editor
+{
 	
 	//! Minimum space between nodes
-	const int minSpace = 10;
+	const int minSpace = 100;
 	bool foldout = false;
 	
 	/*!
 	 * 	Make a button to (Re)Build all checkpoints. This means calculating all walkable nodes,
 	 * 	then calculating the distance from each node to each checkpoint.
 	 */
-	void OnEnable(){
+	void OnEnable()
+	{
 		foldout = EditorPrefs.GetBool("CheckpointManagerEditor.foldout");
 		//findClosest = EditorPrefs.GetBool("CheckpointManagerEditor.findClosest");
 	}
 	
-	void OnDisable(){
+	void OnDisable()
+	{
 		EditorPrefs.SetBool("CheckpointManagerEditor.foldout", foldout);
 		//EditorPrefs.SetBool("CheckpointManagerEditor.findClosest", findClosest);
 	}
 	
-    public override void OnInspectorGUI() {
-        CheckpointManager myTarget = (CheckpointManager)target;
-		if (GUILayout.Button ("Save Tree as Json")) {
-			myTarget.checkpointTree.SaveTreeAsJson();
-		}
-		if (GUILayout.Button ("(Re)Build checkpoints")) {
+	public override void OnInspectorGUI()
+	{
+		//Set reference to CheckpointManager class
+		CheckpointManager myTarget = (CheckpointManager)target;
+		
+		//(Re)Build checkpoints button
+		string s = (myTarget.checkpointTree == null) ? "Build checkpoints" : "Rebuild checkpoints";
+		if (GUILayout.Button(s)) {
 			BuildNodeTree(myTarget);
-        }
-		EditorGUILayout.Space();
+		}
+		
+		//Save checkpoints button
+		if (myTarget.checkpointTree == null) {
+			GUI.enabled = false;
+			GUILayout.Button("Please build checkpoints first");
+			GUI.enabled = true;
+		} else if (GUILayout.Button("Save checkpoints to file")) {
+			if (myTarget.checkpointTree.SaveTreeAsJson(Application.dataPath + "/Resources/Json/checkpointData.json")) {
+				Log.M("checkpoint", "Checkpoints saved.");
+			} else {
+				Log.M("checkpoint", "Saving checkpoints failed.");
+			}
+		}
+		
+		//Load checkpoints button
+		if (GUILayout.Button("Load checkpoints from file")) {
+			if (myTarget.checkpointTree == null) {
+				LoadCheckpoints(myTarget);
+			} else if (EditorUtility.DisplayDialog("Continue load from file",
+			                                       "Checkpoint data already exists. Old data will be wiped.",
+			                                       "Continue",
+			                                       "Cancel")) {
+				LoadCheckpoints(myTarget);
+			}
+		}
+		
 		EditorGUILayout.Space();
 		EditorGUILayout.Space();
 		
 		EditorGUILayout.HelpBox("Will Color Closest Checkpoint Red", MessageType.Info);
 		myTarget.pos = EditorGUILayout.Vector3Field("Pos", myTarget.pos);
-		if (GUILayout.Button ("Find closest checkpoint") && !EditorApplication.isPlaying) {
+		if (GUILayout.Button("Find closest checkpoint") && !EditorApplication.isPlaying) {
 			List<Vector3> tpmp = GameObject.Find("CheckpointManager").GetComponent<CheckpointManager>().checkpointTree.Search(myTarget.pos);
 			
-			if(tpmp != null && tpmp.Count != 0){
-				myTarget.closestCheckpoint = tpmp[0];
+			if (tpmp != null && tpmp.Count != 0) {
+				myTarget.closestCheckpoint = tpmp [0];
 				myTarget.findClosest = true;
-				Log.M ("checkpoint", "Closest checkpoint: "+myTarget.closestCheckpoint);
-				foreach(Vector3 v in tpmp){
+				Log.M("checkpoint", "Closest checkpoint: " + myTarget.closestCheckpoint);
+				foreach (Vector3 v in tpmp) {
 					//Log.M ("checkpoint", ""+v);
 				}
-			}
-			else
+			} else {
 				Log.M("checkpoint", "Un-walkable position.");
+			}
 			SceneView.RepaintAll();
 			
 		}
 		
 		//Display all checkpoint locations
-		EditorGUILayout.LabelField ("Checkpoints", myTarget.checkpointList.Count.ToString());
-        EditorGUILayout.Space();
+		EditorGUILayout.LabelField("Checkpoints", myTarget.checkpointList.Count.ToString());
+		EditorGUILayout.Space();
 		foldout = EditorGUILayout.Foldout(foldout, "Display checkpoints");
-		if(foldout){
+		if (foldout) {
 			foreach (Vector3 v in myTarget.checkpointList) {
-	            EditorGUILayout.BeginHorizontal();
-	            EditorGUILayout.LabelField ("X",GUILayout.MaxWidth (20));
-	            EditorGUILayout.LabelField (v.x.ToString ("0.0"),GUILayout.MaxWidth (80));
-	            EditorGUILayout.LabelField ("Z",GUILayout.MaxWidth (20));
-	            EditorGUILayout.LabelField (v.z.ToString ("0.0"));
-	            EditorGUILayout.EndHorizontal();
-	        }
-        }
-	}
-	
-	void FinishDialogueBox(int checkpoints, Vector2 min, Vector2 max, int nodes){
-		if(EditorUtility.DisplayDialog("(Re)Building Checkpoints complete!",
-		                               "[Checkpoints count is " + checkpoints +"]\n" +
-		                               "[Max bounds are "+max+"]\n" +
-		                               "[Min bounds are "+min+"]\n" +
-		                               "[Node count is "+nodes+"]\n" +
-		                               "[Total connections count is "+nodes*checkpoints+"]",
-		                               "Continue")){
+				EditorGUILayout.BeginHorizontal();
+				EditorGUILayout.LabelField("X", GUILayout.MaxWidth(20));
+				EditorGUILayout.LabelField(v.x.ToString("0.0"), GUILayout.MaxWidth(80));
+				EditorGUILayout.LabelField("Z", GUILayout.MaxWidth(20));
+				EditorGUILayout.LabelField(v.z.ToString("0.0"));
+				EditorGUILayout.EndHorizontal();
+			}
 		}
-		Repaint();
 	}
 
-	void BuildNodeTree(CheckpointManager myTarget){
+	void LoadCheckpoints(CheckpointManager myTarget)
+	{
+		myTarget.checkpointTree = new NodeTree();
+		if (myTarget.checkpointTree.LoadTreeFromFile(Application.dataPath + "/Resources/Json/checkpointData.json")) {
+			Log.M("checkpoint", "Checkpoints loaded.");
+		} else {
+			Log.M("checkpoint", "Loading checkpoints failed.");
+		}
+	}
+		
+	void BuildNodeTree(CheckpointManager myTarget)
+	{
 		//find all gameobjects with the <checkpoint> and place (positions) into List<Vector3>
-		Checkpoint[] cps = Object.FindObjectsOfType (typeof (Checkpoint)) as Checkpoint[];
+		Checkpoint[] cps = Object.FindObjectsOfType(typeof(Checkpoint)) as Checkpoint[];
 		myTarget.checkpointList.Clear();
 		foreach (Checkpoint cp in cps) {
-			myTarget.checkpointList.Add (new Vector3 (cp.transform.position.x,0,cp.transform.position.z));
+			myTarget.checkpointList.Add(new Vector3(cp.transform.position.x, 0, cp.transform.position.z));
 		}
-		if (myTarget.checkpointList.Count == 0){
-			Log.W ("editor", "No Checkpoints in scene");
+		if (myTarget.checkpointList.Count == 0) {
+			Log.W("editor", "No Checkpoints in scene");
 			return;
 		}
 		
@@ -99,20 +128,21 @@ public class CheckpointManagerEditor : Editor {
 		Vector2 min = new Vector2();
 		Vector2 max = new Vector2();
 		GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>() as GameObject[];
-		foreach (GameObject go in allObjects)
-		if (go.activeInHierarchy) {
-			if(go.GetComponent<Renderer>()){
-				Bounds pos = go.GetComponent<Renderer>().bounds;
-				min.x = Mathf.Min (pos.min.x, min.x);
-				min.y = Mathf.Min (pos.min.z, min.y);
-				max.x = Mathf.Max (pos.max.x, max.x);
-				max.y = Mathf.Max (pos.max.z, max.y);
-			} else {
-				Vector3 pos = go.transform.position;
-				min.x = Mathf.Min (pos.x, min.x);
-				min.y = Mathf.Min (pos.z, min.y);
-				max.x = Mathf.Max (pos.x, max.x);
-				max.y = Mathf.Max (pos.z, max.y);
+		foreach (GameObject go in allObjects) {
+			if (go.activeInHierarchy) {
+				if (go.GetComponent<Renderer>()) {
+					Bounds pos = go.GetComponent<Renderer>().bounds;
+					min.x = Mathf.Min(pos.min.x, min.x);
+					min.y = Mathf.Min(pos.min.z, min.y);
+					max.x = Mathf.Max(pos.max.x, max.x);
+					max.y = Mathf.Max(pos.max.z, max.y);
+				} else {
+					Vector3 pos = go.transform.position;
+					min.x = Mathf.Min(pos.x, min.x);
+					min.y = Mathf.Min(pos.z, min.y);
+					max.x = Mathf.Max(pos.x, max.x);
+					max.y = Mathf.Max(pos.z, max.y);
+				}
 			}
 		}
 		
@@ -120,8 +150,9 @@ public class CheckpointManagerEditor : Editor {
 		//todo: include ability to filer out regions that are not walk/playable
 		List<Vector3> nodes = new List<Vector3>();
 		for (float i = min.x; i < max.x; i+=minSpace) {
-			for (float j = min.y; j < max.y; j+=minSpace)
-				nodes.Add (new Vector3 (i,0,j));
+			for (float j = min.y; j < max.y; j+=minSpace) {
+				nodes.Add(new Vector3(i, 0, j));
+			}
 		}
 		
 		//Calculate the distances frome each checkpoint to each node. Unity (player) must be running to use NavMesh functions
@@ -134,31 +165,31 @@ public class CheckpointManagerEditor : Editor {
 		float tempFloat;
 		Vector3 tempV3 = new Vector3();
 
-		foreach (Vector3 n in nodes){
+		foreach (Vector3 n in nodes) {
 			//Calculate distance from node to each checkpoint
 			checkpointDict.Clear();
-			foreach(Vector3 c in myTarget.checkpointList){
+			foreach (Vector3 c in myTarget.checkpointList) {
 				tempFloat = 0.0f; //use to measure disatance here
-				NavMesh.CalculatePath (n,c, -1, path);
+				NavMesh.CalculatePath(n, c, -1, path);
 				
-				if(path.corners.Length != 0){
-					tempV3 = path.corners[0]; //used to hold last V3 value here
-					foreach (Vector3 v in path.corners){
-						tempFloat = tempFloat + Vector3.Distance(tempV3,v);
+				if (path.corners.Length != 0) {
+					tempV3 = path.corners [0]; //used to hold last V3 value here
+					foreach (Vector3 v in path.corners) {
+						tempFloat = tempFloat + Vector3.Distance(tempV3, v);
 						
 						tempV3 = v;
 						//Log.M ("editor","[Remaining distance "+v+"]");
 					}
-					checkpointDict.Add(c,tempFloat);
+					checkpointDict.Add(c, tempFloat);
 				}
 			}
 
 			//Sort checkpoint list by shortest distance
 			List<Vector3> checkpointList = new List<Vector3>();	//Temporary list of checkpoints sorted by shortest distance from the node
-			for(int i = 0; i <checkpointDict.Count; i++){
+			for (int i = 0; i <checkpointDict.Count; i++) {
 				tempFloat = float.MaxValue; //used to find smallest value in dictionary here
-				foreach(KeyValuePair<Vector3, float> p in checkpointDict){
-					if(!checkpointList.Contains(p.Key) && p.Value < tempFloat){
+				foreach (KeyValuePair<Vector3, float> p in checkpointDict) {
+					if (!checkpointList.Contains(p.Key) && p.Value < tempFloat) {
 						tempFloat = p.Value;
 						tempV3 = p.Key; //used to hold key
 					}
@@ -175,8 +206,8 @@ public class CheckpointManagerEditor : Editor {
 		Vector3 fucks = new Vector3();
 		//Put all node data and checkpoint paths into NodeTree
 		Dictionary<Vector3,List<Vector3>> dict = new Dictionary<Vector3,List<Vector3>>();
-		for(int i=0;i<nodes.Count;i++){
-			dict.Add(nodes[i],nodeList[i]);
+		for (int i=0; i<nodes.Count; i++) {
+			dict.Add(nodes [i], nodeList [i]);
 		}
 
 		myTarget.checkpointTree = new NodeTree(dict);
@@ -186,6 +217,19 @@ public class CheckpointManagerEditor : Editor {
 		
 		//Pull up dialogue box with results
 		FinishDialogueBox(myTarget.checkpointList.Count, min, max, nodes.Count);
+	}
+	
+	void FinishDialogueBox(int checkpoints, Vector2 min, Vector2 max, int nodes)
+	{
+		if (EditorUtility.DisplayDialog("(Re)Building Checkpoints complete!",
+		                                "[Checkpoints count is " + checkpoints + "]\n" +
+			"[Max bounds are " + max + "]\n" +
+			"[Min bounds are " + min + "]\n" +
+			"[Node count is " + nodes + "]\n" +
+			"[Total connections count is " + nodes * checkpoints + "]",
+		                                "Continue")) {
+		}
+		Repaint();
 	}
 }
 
--- Assets/Scripts/SerializationManager/Checkpoints/NodeTree.cs
+++ Assets/Scripts/SerializationManager/Checkpoints/NodeTree.cs
@@ -14,13 +14,20 @@ public class NodeTree
 	
 	//Global variables used to calculate averages of entire list of nodes,then of each cartesian quadrant's nodes
 	float x, z;
-	int numNodes;
+	int totalNodes;
 
-	//! Constructor \todo implementation + code
-	public NodeTree(Dictionary<Vector3,List<Vector3>> nodes){
-		numNodes = nodes.Count;
-		m_root = new Node(new Vector3(x / numNodes, 0, z / numNodes), null, null, null, null, true, null);
+	//! COnstructor with blank data
+	public NodeTree()
+	{
 
+	}
+
+	//! Constructor using dictionary
+	public NodeTree(Dictionary<Vector3,List<Vector3>> nodes)
+	{
+		totalNodes = nodes.Count;
+		m_root = new Node(new Vector3(x / nodes.Count, 0, z / nodes.Count), null, null, null, null, null);
+		
 		// Send list to recursive insert function
 		RecursiveInsert(nodes);
 	}
@@ -35,20 +42,24 @@ public class NodeTree
 		public Node child_2;
 		public Node child_3;
 		public Node child_4;
-		public bool isEmpty;	/*!<Indicates whether node is an empty Node because not able to set a vector to null */
 		public List<Vector3> checkpoints;
 		
 		//node constructor
-		public Node(Vector3 n_location, Node n_child_1, Node n_child_2, Node n_child_3, Node n_child_4,
-		            bool n_isEmpty, List<Vector3> n_checkpoints)
+		public Node(Vector3 n_location, Node n_child_1, Node n_child_2, Node n_child_3, Node n_child_4, List<Vector3> n_checkpoints)
 		{
 			location = n_location;
 			child_1 = n_child_1;
 			child_2 = n_child_2;
 			child_3 = n_child_3;
 			child_4 = n_child_4;
-			isEmpty = n_isEmpty;
 			checkpoints = n_checkpoints;
+		}
+		
+		
+		//! Determine if the Node is empty by determining if the checkpoints list exists
+		public bool isEmpty()
+		{
+			return (checkpoints == null) ? true : false;
 		}
 	};
 	
@@ -63,7 +74,7 @@ public class NodeTree
 		}
 
 		// passing in node list, average x location, average z location, and root of tree
-		RecursiveInsert(nodes, x / numNodes, z / numNodes, m_root);
+		RecursiveInsert(nodes, x / nodes.Count, z / nodes.Count, m_root);
 	}
 
 	/*!
@@ -83,13 +94,13 @@ public class NodeTree
 			
 			foreach (Vector3 node in nodes.Keys) {
 				if (node.x >= xPos && node.z >= zPos) {
-					quadrant_1.Add(node,nodes[node]);
+					quadrant_1.Add(node, nodes [node]);
 				} else if (node.x < xPos && node.z >= zPos) {
-					quadrant_2.Add(node,nodes[node]);
+					quadrant_2.Add(node, nodes [node]);
 				} else if (node.x < xPos && node.z < zPos) {
-					quadrant_3.Add(node,nodes[node]);
+					quadrant_3.Add(node, nodes [node]);
 				} else {
-					quadrant_4.Add(node,nodes[node]);
+					quadrant_4.Add(node, nodes [node]);
 				}
 			}
 			// All four segements of the interal code (quadrant_1, quadrant_2, etc.) are needed as C# pointers cannot be used in a safe-only environment, Unity enforced.
@@ -100,23 +111,21 @@ public class NodeTree
 			if (quadrant_1.Count > 1) {
 				x = 0;
 				z = 0;
-				numNodes = 0;
 				// calculate average of this cartesian quadrant; greater greater
 				foreach (Vector3 q1 in quadrant_1.Keys) {
 					x += q1.x;
 					z += q1.z;
-					numNodes++;
 				}
 				// Add empty node to tree
-				parent.child_1 = new Node(new Vector3(x / numNodes, 0, z / numNodes), null, null, null, null, true, null);
+				parent.child_1 = new Node(new Vector3(x / quadrant_1.Count, 0, z / quadrant_1.Count), null, null, null, null, null);
 
 				// Recurse by passing in this quadrant's node list, this quadrant's average x location, this quadrant's average z location, firstChild because this is first quadrant
-				RecursiveInsert(quadrant_1, x / numNodes, z / numNodes, parent.child_1);
+				RecursiveInsert(quadrant_1, x / quadrant_1.Count, z / quadrant_1.Count, parent.child_1);
 			}
 			// If list only has one node, push node onto tree
 			else if (quadrant_1.Count == 1) {
 				foreach (Vector3 q1 in quadrant_1.Keys) {
-					parent.child_1 = new Node(q1, null, null, null, null, false, quadrant_1[q1]);
+					parent.child_1 = new Node(q1, null, null, null, null, quadrant_1 [q1]);
 				}
 			}
 			#endregion
@@ -127,23 +136,21 @@ public class NodeTree
 			if (quadrant_2.Count > 1) {
 				x = 0;
 				z = 0;
-				numNodes = 0;
 				// calculate average of this cartesian quadrant; greater less
 				foreach (Vector3 q2 in quadrant_2.Keys) {
 					x += q2.x;
 					z += q2.z;
-					numNodes++;
 				}
 				// Add empty node to tree
-				parent.child_2 = new Node(new Vector3(x / numNodes, 0, z / numNodes), null, null, null, null, true, null);
+				parent.child_2 = new Node(new Vector3(x / quadrant_2.Count, 0, z / quadrant_2.Count), null, null, null, null, null);
 
 				// Recurse by passing in this quadrant's nodes list, this quadrant's average x location, this quadrant's average z location, secondChild because this is second quadrant
-				RecursiveInsert(quadrant_2, x / numNodes, z / numNodes, parent.child_2);
+				RecursiveInsert(quadrant_2, x / quadrant_2.Count, z / quadrant_2.Count, parent.child_2);
 			}
 			// If list only has one node, push node onto tree
 			else if (quadrant_2.Count == 1) {
 				foreach (Vector3 q2 in quadrant_2.Keys) {
-					parent.child_2 = new Node(q2, null, null, null, null, false, quadrant_2[q2]);
+					parent.child_2 = new Node(q2, null, null, null, null, quadrant_2 [q2]);
 				}
 			}
 			#endregion
@@ -154,23 +161,21 @@ public class NodeTree
 			if (quadrant_3.Count > 1) {
 				x = 0;
 				z = 0;
-				numNodes = 0;
 				// calculate average of this cartesian quadrant; less greater
 				foreach (Vector3 q3 in quadrant_3.Keys) {
 					x += q3.x;
 					z += q3.z;		
-					numNodes++;
 				}
 				// Add empty node to tree
-				parent.child_3 = new Node(new Vector3(x / numNodes, 0, z / numNodes), null, null, null, null, true, null);
+				parent.child_3 = new Node(new Vector3(x / quadrant_3.Count, 0, z / quadrant_3.Count), null, null, null, null, null);
 
 				// Recurse by passing in this quadrant's node list, this quadrant's average x location, this quadrant's average z location, secondChild because this is third quadrant
-				RecursiveInsert(quadrant_3, x / numNodes, z / numNodes, parent.child_3);
+				RecursiveInsert(quadrant_3, x / quadrant_3.Count, z / quadrant_3.Count, parent.child_3);
 			}
 			// If list only has one node, push node onto tree
 			else if (quadrant_3.Count == 1) {
 				foreach (Vector3 q3 in quadrant_3.Keys) {
-					parent.child_3 = new Node(q3, null, null, null, null, false, quadrant_3[q3]);
+					parent.child_3 = new Node(q3, null, null, null, null, quadrant_3 [q3]);
 				}
 			}
 			#endregion
@@ -181,23 +186,21 @@ public class NodeTree
 			if (quadrant_4.Count > 1) {
 				x = 0;
 				z = 0;
-				numNodes = 0;
 				// calculate average of this cartesian quadrant; less less
 				foreach (Vector3 q4 in quadrant_4.Keys) {
 					x += q4.x;
 					z += q4.z;
-					numNodes++;
 				}
 				// Add empty node onto tree
-				parent.child_4 = new Node(new Vector3(x / numNodes, 0, z / numNodes), null, null, null, null, true, null);
+				parent.child_4 = new Node(new Vector3(x / quadrant_4.Count, 0, z / quadrant_4.Count), null, null, null, null, null);
 
 				// Recurse by passing in this quadrant's node list, this quadrant's average x location, this quadrant's average z location, secondChild because this is fourth quadrant
-				RecursiveInsert(quadrant_4, x / numNodes, z / numNodes, parent.child_4);
+				RecursiveInsert(quadrant_4, x / quadrant_4.Count, z / quadrant_4.Count, parent.child_4);
 			}
 			// If list only has one node, push node onto tree
 			else if (quadrant_4.Count == 1) {
 				foreach (Vector3 q4 in quadrant_4.Keys) {
-					parent.child_4 = new Node(q4, null, null, null, null, false, quadrant_4[q4]);
+					parent.child_4 = new Node(q4, null, null, null, null, quadrant_4 [q4]);
 				}
 			}
 			#endregion
@@ -207,7 +210,8 @@ public class NodeTree
 	//! Search tree for closest node to the Vector3
 	public List<Vector3> Search(Vector3 loc)
 	{
-		if (m_root != null) {;
+		if (m_root != null) {
+			;
 			return Search(m_root, loc, null);
 		} else {
 			return null;
@@ -218,143 +222,153 @@ public class NodeTree
 	private List<Vector3> Search(Node n, Vector3 loc, List<Vector3> nodeList)
 	{
 		//If we hit the bottom line of nodes
-		if (n != null && n.isEmpty == false) {
+		if (n != null && !n.isEmpty()) {
 			return n.checkpoints;
 		}
 		if (n.child_1 != null && loc.x >= n.location.x && loc.z >= n.location.z) {
-			return Search (n.child_1, loc, nodeList);
+			return Search(n.child_1, loc, nodeList);
 		} else if (n.child_2 != null && loc.x < n.location.x && loc.z >= n.location.z) {
-			return Search (n.child_2, loc, nodeList);
+			return Search(n.child_2, loc, nodeList);
 		} else if (n.child_3 != null && loc.x < n.location.x && loc.z < n.location.z) {
-			return Search (n.child_3, loc, nodeList);
+			return Search(n.child_3, loc, nodeList);
 		} else if (n.child_4 != null && loc.x >= n.location.x && loc.z < n.location.z) {
-			return Search (n.child_4, loc, nodeList);
+			return Search(n.child_4, loc, nodeList);
 		} else {
 			return null;
 		}
 	}
 	
-	public string SaveTreeAsJson(){
+	/*!
+	 *	This will print the NodeTree as a .json string. The string can then be saved and used later.
+	 *	Empty nodes will have a location and 1-4 children
+	 *	End nodes will have a location and an array op checkpoints
+	 */
+	public bool SaveTreeAsJson(string path)
+	{
+		System.IO.File.Delete(path);
+		System.IO.File.WriteAllText(path, GetTreeAsJson());
+		return System.IO.File.Exists(path);
+	}
+	
+	public string GetTreeAsJson()
+	{
 		string s = "";
-		//Opening json bracket and enter numNodes
-		s+="{\"numNodes\":"+ numNodes +",";
+		//Opening json bracket and enter totalNodes
+		s += "{\"totalNodes\":" + totalNodes + ",";
 		
-		if(m_root.child_1 != null && m_root.child_2 != null && m_root.child_3 != null && m_root.child_4 != null){
-			s+= GetNodeData(m_root);
-		}else{
-			Log.E("checkpoint","NodeTree empty. Please (Re)Build Checkpoints.");
+		if (m_root.child_1 != null && m_root.child_2 != null && m_root.child_3 != null && m_root.child_4 != null) {
+			s += "\"node\":{" + GetNodeData(m_root);
+		} else {
+			Log.E("checkpoint", "NodeTree empty. Please (Re)Build Checkpoints.");
 		}
 		
 		//Close out json bracket
-		s+="}";
+		s += "}";
+		
+		//Log.M("checkpoint", "Checkpoint string in .json format:\n"+s);
 		return s;
 	}
 	
-	private string GetNodeData(Node n){
+	int i = 0;
+	private string GetNodeData(Node n)
+	{
 		string s = "";
 		bool comma = false;	//This is used to tell the next segment to make a comma (if there was a previous segment)
 		//While node has children
-		Debug.Log("cunt");
-		s+="\"node\":{";
-		comma = false;
 		
-		while(n.isEmpty == true){
-			s+="\"location\":\"null\",";
-			if(n.child_1 != null){
-				s+=GetNodeData(n.child_1);
+		if (n.isEmpty()) {
+			//s += "\"location\":\"null\",";
+			s += "\"location\":\"" + n.location + "\",";
+			if (n.child_1 != null) {
+				s += "\"child_1\":{" + GetNodeData(n.child_1);
 				comma = true;
 			}
-			if(n.child_2 != null){
-				if(comma == true){
-					s+=",";
+			if (n.child_2 != null) {
+				if (comma == true) {
+					s += ",";
 					comma = false;
 				}
-				s+=GetNodeData(n.child_2);
+				s += "\"child_2\":{" + GetNodeData(n.child_2);
 				
 				comma = true;
 			}
-			if(n.child_3 != null){
-				if(comma == true){
-					s+=",";
+			if (n.child_3 != null) {
+				if (comma == true) {
+					s += ",";
 					comma = false;
 				}
-				s+=GetNodeData(n.child_3);
+				s += "\"child_3\":{" + GetNodeData(n.child_3);
 				
 				comma = true;
 			}
-			if(n.child_4 != null){
-				if(comma == true){
-					s+=",";
+			if (n.child_4 != null) {
+				if (comma == true) {
+					s += ",";
 					comma = false;
 				}
-				s+=GetNodeData(n.child_4);
+				s += "\"child_4\":{" + GetNodeData(n.child_4);
 				
 				comma = true;
 			}
-			s+="\"isEmpty\":"+n.isEmpty.ToString();
-		}
-		while(n.isEmpty == false){
-			if(n.child_1 != null){
-				s+=CreateNodeBaseStr(1, n.child_1);
-				comma = true;
-			}
-			if(n.child_2 != null){
-				if(comma == true){
-					s+=",";
-					comma = false;
+			//s += ",\"isEmpty\":" + n.isEmpty.ToString().ToLower();
+		} else if (!n.isEmpty()) {
+			s += "\"location\":\"" + n.location.ToString() + "\",";
+			//s += "\"isEmpty\":" + n.isEmpty.ToString().ToLower() + ",";
+			s += "\"checkpoints\":[";
+			bool once = true;
+			if (n.checkpoints != null) {
+				foreach (Vector3 v in n.checkpoints) {
+					if (once) {
+						s += "\"" + v.ToString() + "\"";
+						once = false;
+					} else {
+						s += ",\"" + v.ToString() + "\"";
+					}
 				}
-				s+=CreateNodeBaseStr(2, n.child_2);
-				
-				comma = true;
 			}
-			if(n.child_3 != null){
-				if(comma == true){
-					s+=",";
-					comma = false;
-				}
-				s+=CreateNodeBaseStr(3, n.child_3);
-				
-				comma = true;
-			}
-			if(n.child_4 != null){
-				if(comma == true){
-					s+=",";
-					comma = false;
-				}
-				s+=CreateNodeBaseStr(4, n.child_4);
-				
-				comma = true;
-			}
-		}
-		s+="}";
-		return s;
-	}
-	
-	private string CreateNodeBaseStr(int i, Node n){
-		string s = "\"child_"+i+"\":{";
-		s+="\"location\":\""+n.location.ToString()+"\",";
-		s+="\"isEmpty\":"+n.isEmpty.ToString()+",";
-		s+="\"checkpoints\":[";
-		bool once = true;
-		foreach(Vector3 v in n.checkpoints){
-			if(once){
-				s+="\""+v.ToString()+"\"";
-			}else{
-				s+=",\""+v.ToString()+"\"";
-			}
-		}
-		s+="]";
-		
-		s+= "}";
-		return s;
-	}
-	
-	public void LoadStringFromJson(){
-		string text = System.IO.File.ReadAllText (Application.dataPath + "/Resources/Json/checkpointData.json");
-		var N = JSON.Parse (text);
-//		for (int i = 0; i < N.Count; i++)
-//			logStrings.Add (N [i].Value);
+			s += "]";
 			
-		m_root = new Node(new Vector3(x / numNodes, 0, z / numNodes), null, null, null, null, true, null);
+		}
+		s += "}";
+		return s;
+	}
+	
+	private string CreateNodeBaseStr(int i, Node n)
+	{
+		string s = "\"child_" + i + "\":{";
+		s += "\"location\":\"" + n.location.ToString() + "\",";
+		//s += "\"isEmpty\":" + n.isEmpty.ToString().ToLower() + ",";
+		s += "\"checkpoints\":[";
+		bool once = true;
+		if (n.checkpoints != null) {
+			foreach (Vector3 v in n.checkpoints) {
+				if (once) {
+					s += "\"" + v.ToString() + "\"";
+					once = false;
+				} else {
+					s += ",\"" + v.ToString() + "\"";
+				}
+			}
+		}
+		s += "]";
+		
+		s += "}";
+		return s;
+	}
+	
+	public bool LoadTreeFromFile(string path)
+	{
+		if (!System.IO.File.Exists(path)) {
+			Log.E("checkpoint", "File does not exist: " + path);
+			return false;
+		}
+		
+		string checkpointData = System.IO.File.ReadAllText(path);
+		var N = JSON.Parse(checkpointData);
+		//		for (int i = 0; i < N.Count; i++)
+		//			logStrings.Add (N [i].Value);
+		
+		//m_root = new Node(new Vector3(x / totalNodes, 0, z / totalNodes), null, null, null, null, null);
+		return true;
 	}
 }
