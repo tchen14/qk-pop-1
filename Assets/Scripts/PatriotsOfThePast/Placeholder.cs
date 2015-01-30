28b0d8080d0139118c12c5203540b1d93d6f36df
Jordan Emmanuel <fat_flying_pigs@yahoo.com>
2015-01-30 12:28:13 -0800

    Fixing merge conflicts (duplicate files)

--- Assets/Scripts/PatriotsOfThePast/Placeholder.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public abstract class Placeholder : MonoBehaviour {
-
-    public string id = "";	//this variable is for use with quest checkpoints
-}
