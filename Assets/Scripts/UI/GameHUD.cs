1c2f79aef8206304aeca061b62ef1e2cd150c293
Student_QN_00 <cagd495@mailinator.com>
2015-02-12 15:41:37 -0800

    Started HUD

---
+++ Assets/Scripts/UI/GameHUD.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameHUD : MonoBehaviour {
+
+	public GameObject gameMap;
+	public GameObject player;
+	GameObject objectiveText;
+
+	void Start () {
+		objectiveText = GameObject.Find ("objectiveText");
+	}
+	
+
+	void Update () {
+		UpdateMap ();
+
+	}
+
+	public void UpdateMap () {
+
+		Vector3 newMapPos = player.transform.position;
+		newMapPos.y = player.transform.position.z;
+		gameMap.GetComponent<RectTransform>().localPosition = newMapPos;
+	}
+
+	public void UpdateObjectiveText (string newObjective) {
+		objectiveText.GetComponent<Text> ().text = "Objective: " + newObjective;
+	}
+}
