07a274b36b31dbeec626cfd65d0ce7edf817ad8f
Jordan Emmanuel <fat_flying_pigs@yahoo.com>
2015-03-02 06:23:40 -0800

    Grounded fixed, variable cleanup, running and crouching implemented

--- Assets/Scripts/CharacterController/CharacterController_2.cs
+++ Assets/Scripts/CharacterController/CharacterController_2.cs
@@ -3,74 +3,59 @@ using System.Collections;
 using System.Collections.Generic;
 
 [RequireComponent (typeof (Rigidbody))]
-[RequireComponent (typeof (Collider))]
+[RequireComponent (typeof (CapsuleCollider))]
 /*!
  *	Base class for a characterController. The _2 is appanded due to Unity's built in CharacterController class
  */
 public abstract class CharacterController_2 : MonoBehaviour {
 	
-	//! Movement variables
+	//! Overlord variables
+	protected CapsuleCollider myCollider;
+	
+	// Movement variables
 	protected Vector3 targetDirection = Vector3.zero;
-	[SerializeField] protected float minMovementSpeed = 10f;
-	[SerializeField] protected float maxMovementSpeed = 20f;
-	[SerializeField] protected float acceleration = 7.5f;
-	[SerializeField] protected float deceleration = 20.0f;
-	[SerializeField] public float currentMovementSpeed;
-	public float rotationSpeed = 3.5f;
+	protected float currentWalkingSpeed;
+	public const float rotationSpeed = 3.5f;
+	protected const float minWalkingSpeed = 10f;
+	protected const float maxWalkingSpeed = 20f;
+	protected const float runningSpeedMod = 1.2f;
+	protected const float moveAcceleration = 7.5f;
+	protected const float moveDeceleration = 20.0f;
 	
 	// Jumping variables
-	public Vector3 jumpDirection = Vector3.zero;
-	[SerializeField] protected float maxJumpingHeight = 0.5f;
-	[SerializeField] protected float airMovementSpeedPercentage = 0.1f;
+	[ReadOnly] public bool grounded = false; //Only public for inspector. Should be protected
+	protected const float maxJumpingHeight = 0.5f;
+	protected const float airMovementSpeedPercentage = 0.1f;
+	
+	// Crouching variables
+	protected const float crouchingMod = 0.5f;
 	
 	// Climbing variables
-	[SerializeField] protected float minClimbingSpeed = 5f;
-	[SerializeField] protected float maxClimbingSpeed = 10f;
-	[SerializeField] public float currentClimbingSpeed;
-	[SerializeField] protected float ladderClimbingSpeed = 200f;
+	[ReadOnly] public float currentClimbingSpeed;
+	protected const float minClimbingSpeed = 5f;
+	protected const float maxClimbingSpeed = 10f;
+	
+	// Modifiers
+	[ReadOnly] public bool isRunning = false; //Only public for inspector. Should be protected
+	[ReadOnly] public bool isCrouched = false; //Only public for inspector. Should be protected
 
 	// Character states
-	public enum characterState { idleState, movingState, walkingState, runningState, jumpingState, movingJumpState, climbingState, endClimbingState}
+	public enum characterState { idleState, movingState, jumpingState, movingJumpState, climbingState, endClimbingState}
 	public characterState currentCharacterState = characterState.idleState;
 	
-	//Only public for inspector. Should be protected
-	[ReadOnly] public bool moving;
-	[ReadOnly] public bool grounded;
-	
-	private int frameBuffer = 3;
-	private List<float> _grounded;
-	private List<Vector2> _moving;
-
-	protected virtual void Start(){
-		_grounded = new List<float>();
-		_moving = new List<Vector2>();
+	public virtual void Start(){
+		myCollider = GetComponent<CapsuleCollider>();
 	}
-
-	//!Unity built in function, updates at a fixed interval. Used to calculated isGrounded and isMoving
-	protected virtual void FixedUpdate(){
-		//sets grounded
-		if(_grounded.Count >= frameBuffer)
-			_grounded.RemoveAt(0);
-		_grounded.Add(Mathf.Round(transform.position.y * 100f) / 100f);
-		
-		grounded = false;
-		for (int i = 0; i < _grounded.Count - 2; i++) {
-			grounded ^= !Mathf.Approximately(_grounded [i],_grounded[i+1]);
-		}
-		grounded ^= true;
-		
-		//sets moving
-		if(_moving.Count >= frameBuffer)
-			_moving.RemoveAt(0);
-		_moving.Add(new Vector2(Mathf.Round(transform.position.x * 100f) / 100f,
-		                        Mathf.Round(transform.position.z * 100f) / 100f));
-		
-		moving = false;
-		for (int i = 0; i < _moving.Count - 2; i++) {
-			moving ^= !Mathf.Approximately(_moving [i].x,_moving[i+1].x);
-			moving ^= !Mathf.Approximately(_moving [i].y,_moving[i+1].y);
-		}
-		//moving ^= true;
-		//Debug.Log(grounded + "   " + moving);
+	
+	//! Detect if Character is grounded (true)
+	void OnCollisionEnter(Collision collisionInfo){
+		if(collisionInfo.collider.tag == "Navigation")
+			grounded = true;
+	}
+	
+	//! Detect if Character is grounded (false)
+	void OnCollisionExit(Collision collisionInfo){
+		if(collisionInfo.collider.tag == "Navigation")
+			grounded = false;
 	}
 }
