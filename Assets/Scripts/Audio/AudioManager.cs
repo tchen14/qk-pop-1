edda1b0d4d9d86ab5bd6edf29a8052da1c4e8700
CAGD Student <cagd495@mailinator.com>
2015-03-26 16:08:15 -0700

    Started AudioManager, singleton added

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -4,12 +4,44 @@ using System.Collections;
 /*!
  *	Manages audio for all levels. This class pretty much just loads and plays sounds, on a (timed or untimed) repeat if desired.
  *	\todo implementation + code
+ *	written by Ace
  */
 public class AudioManager : MonoBehaviour {
 
-    //! Unity Start function
+	#region singletonEnforcement
+	private static AudioManager _instance;
+
+	public static AudioManager instance {
+		get {
+			if(_instance == null) {
+				_instance = GameObject.FindObjectOfType<AudioManager>();
+
+				//Dont distroy on new scene load
+				DontDestroyOnLoad(_instance.gameObject);
+			}
+			return _instance;
+		}
+	}
+	#endregion
+
+	//! Unity Start function
     void Start() {
-    }
+		
+		#region singletonCreation
+		if(_instance == null) {
+			//If I am the first instance, make me the Singleton
+			_instance = this;
+			DontDestroyOnLoad(this);
+		} else {
+			//If a Singleton already exists and you find
+			//another reference in scene, destroy it!
+			if(this != _instance)
+				Destroy(this.gameObject);
+		}
+		#endregion
+
+
+	}
 
     //! Unity Update function
     void Update() {
