87d75ab41c40901687337f57b967accdfc5e1e9c
Forrest Spade <fspade@gmail.com>
2015-04-16 14:53:54 -0700

    Recompile Event Library

--- Assets/Scripts/Events/Plugins/EventLibrary.cs
+++ Assets/Scripts/Events/Plugins/EventLibrary.cs
@@ -37,18 +37,22 @@ public static class EventLibrary {
 	public static string[] staticClassesNice = new string[] { "Test Static Class", };
 
 	public static Dictionary<string, string[]> library = new Dictionary<string, string[]> {
+		{ "AIMainMethods", new string[] {"SetAgression", "ChangeNavPoint", } },
+		{ "AudioManagerMethods", new string[] {"changeVol", "seeVol", } },
 		{ "TestScriptLeftMethods", new string[] {"FunctionOne", "FunctionTwo", } },
 		{ "TestScriptRightMethods", new string[] {"NullFunction", "IntFunction", "FloatFunction", "VectorFunction", "GameObjectFunction", } },
 		{ "TestScriptRightStaticMethods", new string[] {"GameObjectFunction", } },
+		{ "AIMainFields", new string[] {"seesTarget", "panic", } },
 		{ "TestScriptLeftFields", new string[] {"counter", "fieldA", "fieldB", "trueFalse", "vectorA", } },
-		{ "CrateFields", new string[] {"isDirty", } },
 	};
 
 	public static Dictionary<string, string[]> libraryNice = new Dictionary<string, string[]> {
+		{ "AIMainMethods", new string[] {"SetAgression", "ChangeNavPoint", } },
+		{ "AudioManagerMethods", new string[] {"changeVol", "seeVol", } },
 		{ "TestScriptLeftMethods", new string[] {"Function One", "FunctionTwo", } },
 		{ "TestScriptRightMethods", new string[] {"NullFunction", "IntFunction", "FloatFunction", "VectorFunction", "GameObjectFunction", } },
 		{ "TestScriptRightStaticMethods", new string[] {"Print \"Debug\"", } },
+		{ "AIMainFields", new string[] {"seesTarget", "panic", } },
 		{ "TestScriptLeftFields", new string[] {"Nice Counter", "fieldA", "fieldB", "trueFalse", "vectorA", } },
-		{ "CrateFields", new string[] {"isDirty", } },
 	};
 }
