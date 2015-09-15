f25d293b85486f273611a48b997f7b5c82bef8a5
CAGD Student <cagd495@mailinator.com>
2015-09-15 16:48:07 -0700

    Began work on enemy aggression meters.

---
+++ Assets/Scripts/UI/enemyBehaviorStatusController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyBehaviorStatusController : MonoBehaviour {
+
+	// Use this for initialization
+	GameObject camera;
+
+	void start(){
+		camera = GameObject.Find("_Main Camera");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.rotation = camera.transform.rotation;
+	}
+}
