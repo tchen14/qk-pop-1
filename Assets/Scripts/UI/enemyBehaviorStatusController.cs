79185ef782ebea2fa20a9cbb1c34c1bdbbe29196
CAGD Student <cagd495@mailinator.com>
2015-09-15 16:48:07 -0700

    Completed the prefab for the meters that indicate an enemies suspicion and aggression levels.
    Signed-off-by: Johnny Thompson <jthompson59@mail.csuchico.edu>

---
+++ Assets/Scripts/UI/enemyBehaviorStatusController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyBehaviorStatusController : MonoBehaviour {
+
+	GameObject mainCamera;
+
+	void Start(){
+		mainCamera = GameObject.Find ("_Main Camera");
+	}
+
+	void Update () {
+		//!Updates the rotation of the indicator bars so they always face the camera.
+		if (mainCamera != null) {
+			this.transform.rotation = mainCamera.transform.rotation;
+		}
+	}
+}
