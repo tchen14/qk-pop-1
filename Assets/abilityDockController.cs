7612092eeff955cfae1b12e7b77c0655363a4222
JohnnyTurbo <jthompson59@mail.csuchico.edu>
2015-10-01 15:49:51 -0700

    Continued work on the ability dock. Was able to make the player scroll through all elements and make a selection.

--- Assets/abilityDockController.cs
+++ Assets/abilityDockController.cs
@@ -8,11 +8,21 @@ public class abilityDockController : MonoBehaviour {
 	public int iconHeight;
 	public int iconSpacing;
 	public int spacingFromBottom;
+	public float lerpSpeed;
+
 	bool dockClosed;
+	bool opening;
+	bool closing;
+	public Vector3[] targetLocation;
+	public Vector3[] startLocation;
 
 	// Use this for initialization
 	void Start () {
+		selectAbility (0);
+		abilities [0].Select ();
+		//openDock ();
 		closeDock ();
+		getStartLocation ();
 	}
 	
 	// Update is called once per frame
@@ -25,26 +35,49 @@ public class abilityDockController : MonoBehaviour {
 				closeDock();
 			}
 		}
+		if (opening) {
+			for(int i = 0; i < abilities.Length; i++){
+				abilities[i].transform.position = Vector3.Lerp(startLocation[i], targetLocation[i], lerpSpeed);
+			}
+		}
 	}
 
 	void closeDock(){
 		for (int i = 0; i < abilities.Length; i++) {
-			abilities[i].transform.position = new Vector3(abilities[i].transform.position.x, 25, 0);
+			//abilities[i].transform.position = new Vector3(abilities[i].transform.position.x, 25, 0);
+			targetLocation[i] = new Vector3(abilities[i].transform.position.x, 25, 0);
+			closing = true;
 		}
 		dockClosed = true;
 	}
 
 	void openDock(){
 		for (int i = 0; i < abilities.Length; i++) {
-			abilities[i].transform.position = new Vector3(abilities[i].transform.position.x, i*(iconHeight + iconSpacing) + spacingFromBottom, 0);
-			//abilities[i].GetComponent<Canvas>().sortingOrder = 0;
+			//abilities[i].transform.position = new Vector3(abilities[i].transform.position.x, i*(iconHeight + iconSpacing) + spacingFromBottom, 0);
+			targetLocation[i] = new Vector3(abilities[i].transform.position.x, i*(iconHeight + iconSpacing) + spacingFromBottom, 0);
+			changeSortOrder(i, 0);
+			opening = true;
 		}
 		dockClosed = false;
 	}
 
-	/*void buttonSelected(int i){
-		abilities[i].GetComponent<Canvas>().sortingOrder = 10;
-		closeDock ();
-	}*/
+	void changeSortOrder(int i, int order){
+		abilities[i].GetComponent<Canvas>().sortingOrder = order;
+	}
 
+	public void selectAbility(int i){
+		if (dockClosed) {
+			openDock ();
+		} else {
+			changeSortOrder (i, 10);
+			closeDock ();
+	
+		}
+	}
+
+	void getStartLocation(){
+		for (int i = 0; i < abilities.Length; i++) {
+			startLocation[i] = abilities[i].transform.position;
+		}
+	}
 }
