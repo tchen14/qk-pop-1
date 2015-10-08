6042fc75ab78116a9e985cce80156b4a6624510a
JohnnyTurbo <jthompson59@mail.csuchico.edu>
2015-10-08 10:35:43 -0700

    Began work on a script that animates the ability dock.

---
+++ Assets/AbilityDockAnimations.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbilityDockAnimations : MonoBehaviour {
+
+	public int[] position;
+	public int numAbilities;
+	
+	void Start () {
+		for(int i = 0; i < numAbilities; i++){
+			position[i] = i;
+		}
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown (KeyCode.DownArrow)){
+			RotateDown ();
+		}
+		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			RotateUp();
+		}
+	}
+	
+	void RotateDown(){
+		for(int i = 0; i < position.Length; i++){
+			position[i] = modulo(position[i] - 1, 5);
+			
+		}
+	}
+	
+	void RotateUp(){
+		for(int i = 0; i < position.Length; i++){
+			position[i] = modulo(position[i] + 1, 5);
+			
+		}
+	}
+	
+	int modulo(int a, int b){
+		return (a%b + b)%b;
+	}
+}
