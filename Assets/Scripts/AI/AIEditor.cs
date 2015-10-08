08f77a92f04711d1e8a03cd0884773f01d3eec9b
CAGD Student <cagd495@mailinator.com>
2015-10-08 14:50:47 -0700

    Adds AI preset swap utility.

--- Assets/Scripts/AI/AIEditor.cs
+++ Assets/Scripts/AI/AIEditor.cs
@@ -15,7 +15,7 @@ public struct AI_Data
 	private float aggressionLimit_;
 	private string panicPoints_;
 	private bool aggression_;
-
+	
 	public AI_Data(int hp,
 	               float sightDistance,
 	               float sightAngle,
@@ -38,7 +38,7 @@ public struct AI_Data
 		panicPoints_ = panicPoints;
 		aggression_ = aggression;
 	}
-
+	
 	public void loadData(AIMain target)
 	{
 		target.hp = hp_;
@@ -55,37 +55,39 @@ public struct AI_Data
 
 [CustomEditor(typeof(AIMain), true)]
 public class AIEditor : Editor {
-
+	
 	AIMain ai_target;
 	AnimBool show_data;
 	string[] ai_types = new string[]{"Villager", "Guard", "Commander"};
-
+	
 	AI_Data[] ai_data = new AI_Data[]{
 		new AI_Data(100, 20, 35, 5, 8, new string[]{"Player"}, 3, 100, "PanicPoints", false),
 		new AI_Data(200, 40, 35, 7, 12, new string[]{"Player"}, 5, 100, "PanicPoints", false),
 		new AI_Data(300, 60, 35, 12, 16, new string[]{"Player"}, 7, 100, "PanicPoints", false)};
-
+	
 	int ai_types_index = 0;
 	int current_selection = 0;
-
+	int current_preset = 0;
+	
 	void OnEnable()
 	{
 		ai_target = (AIMain)target;
+		ai_types_index = ai_target.current_preset;
 		show_data = new AnimBool(false);
 		show_data.valueChanged.AddListener(Repaint);
 	}
-
+	
 	override public void OnInspectorGUI()
 	{
 		EditorGUI.BeginChangeCheck();
-
+		
 		EditorGUILayout.BeginVertical();
 		ai_types_index = EditorGUILayout.Popup ("AI Type:", ai_types_index, ai_types);
-
+		
 		EditorGUILayout.EndVertical();
-
+		
 		show_data.target = EditorGUILayout.Toggle ("Show Data", show_data.target);
-
+		
 		if (EditorGUILayout.BeginFadeGroup (show_data.faded))
 		{
 			EditorGUILayout.LabelField("Health: ",ai_target.hp.ToString() );
@@ -100,19 +102,20 @@ public class AIEditor : Editor {
 			EditorGUILayout.LabelField("Aggression: ",ai_target.aggression.ToString() );
 		}
 		EditorGUILayout.EndFadeGroup();
-
+		
 		if (current_selection != ai_types_index)
 		{
 			loadAI();
 			current_selection = ai_types_index;
+			ai_target.current_preset = ai_types_index;
 		}
 	}
-
+	
 	private void loadAI()
 	{
 		ai_data[ai_types_index].loadData(ai_target);
 	}
-
+	
 	private string print_array(string[] arr){
 		string result = "";
 		foreach(string s in arr){
