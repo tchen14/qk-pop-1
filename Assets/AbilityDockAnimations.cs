2deef28d6f8a700841d15c37bb4e7b94197337be
JohnnyTurbo <jthompson59@mail.csuchico.edu>
2015-10-13 15:28:43 -0700

    Continued work on the ability dock. Hold tab to show ability dock and scroll through items with the up and down arrow keys.

--- Assets/AbilityDockAnimations.cs
+++ Assets/AbilityDockAnimations.cs
@@ -12,30 +12,52 @@ public class AbilityDockAnimations : MonoBehaviour {
 	public float[] targetPosY;
 	public float[] startPosY;
 
+	Image[] abilityDocImages;
 	int numAbilities;
+	int selectedAbility;
 	bool rotating;
 
 	void Start () {
 		numAbilities = position.Length;
+		abilityDocImages = this.gameObject.GetComponentsInChildren<Image>();
+		foreach(Image icon in abilityDocImages){
+			icon.enabled = false;
+		}
 		for(int i = 0; i < numAbilities; i++){
 			position[i] = i;
 		}
+		selectedAbility = position [2];
 	}
 	
 	void Update () {
-		if(Input.GetKeyDown (KeyCode.DownArrow)){
-			newPosUp();
-			//StartCoroutine(rotateCoro());
-			rotate();
-			//rotating = true;
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			foreach(Image icon in abilityDocImages){
+				icon.enabled = true;
+			}
 		}
-		if(Input.GetKeyDown(KeyCode.UpArrow)){
-			newPosDown ();
-			//StartCoroutine(rotateCoro());
-			rotate();
-			//rotating = true;
+		if (Input.GetKeyUp (KeyCode.Tab)) {
+			foreach(Image icon in abilityDocImages){
+				icon.enabled = false;
+			}
+			selectedAbility = position [2];
 		}
-		while (rotating) {
+		if (Input.GetKey(KeyCode.Tab)) {
+			if (Input.GetKeyDown (KeyCode.DownArrow)) {
+				newPosUp ();
+				//rotateDown ();
+				//StartCoroutine(rotateCoroUp());
+				rotate();
+				//rotating = true;
+			}
+			if (Input.GetKeyDown (KeyCode.UpArrow)) {
+				newPosDown ();
+				//rotateUp ();
+				//StartCoroutine(rotateCoroDown());
+				rotate();
+				//rotating = true;
+			}
+		}
+		if (rotating) {
 			for (int i = 0; i < numAbilities; i++) {
 				abilities [i].transform.position = Vector3.Lerp (new Vector3 (abilities [i].transform.position.x, startPosY [i], 0), new Vector3 (abilities [i].transform.position.x, targetPosY [i], 0), lerpTime * Time.deltaTime);
 			}
@@ -58,13 +80,55 @@ public class AbilityDockAnimations : MonoBehaviour {
 		}
 	}
 
-	IEnumerator rotateCoro(){
+	IEnumerator rotateCoroUp(){
 
 			for (int i = 0; i < numAbilities; i++) {
 				//abilities [i].transform.position = Vector3.Lerp (new Vector3 (abilities [i].transform.position.x, startPosY [i], 0), new Vector3 (abilities [i].transform.position.x, targetPosY [i], 0), lerpTime * Time.deltaTime);
 			//abilities[i].transform.Translate(0, 80, 0);
-			abilities[i].rectTransform.Translate(0,80,0);
-				yield return new WaitForSeconds(0.24f);
+			if(position[i] == 0){
+				abilities[i].rectTransform.Translate(0,-320,0);
+			}
+			else{
+				abilities[i].rectTransform.Translate(0,80,0);
+			}
+			//yield return new WaitForSeconds(0.24f);
+			yield return null;
+		}
+	}
+
+	IEnumerator rotateCoroDown(){
+		
+		for (int i = 0; i < numAbilities; i++) {
+			//abilities [i].transform.position = Vector3.Lerp (new Vector3 (abilities [i].transform.position.x, startPosY [i], 0), new Vector3 (abilities [i].transform.position.x, targetPosY [i], 0), lerpTime * Time.deltaTime);
+			//abilities[i].transform.Translate(0, 80, 0);
+			if(position[i] == 4){
+				abilities[i].rectTransform.Translate(0,320,0);
+			}
+			else{
+				abilities[i].rectTransform.Translate(0,-80,0);
+			}
+			yield return null;// new WaitForSeconds(0.24f);
+		}
+	}
+
+	void rotateUp(){
+		for (int i = 0; i < numAbilities; i++) {
+			if(position[i] == 4){
+				abilities[i].rectTransform.Translate(0,320,0);
+			}
+			else{
+				abilities[i].rectTransform.Translate(0,-80,0);
+			}
+		}
+	}
+
+	void rotateDown(){
+		for (int i = 0; i < numAbilities; i++) {
+			if (position [i] == 0) {
+				abilities [i].rectTransform.Translate (0, -320, 0);
+			} else {
+				abilities [i].rectTransform.Translate (0, 80, 0);
+			}
 		}
 	}
 
@@ -93,5 +157,16 @@ public class AbilityDockAnimations : MonoBehaviour {
 		default:
 			return 0;
 		}
+	}
+
+	/* This function returns an integer that corresponds to the ability that is selected
+	 * 0 is Cut
+	 * 1 is Sound Throw
+	 * 2 is Taze
+	 * 3 is Push
+	 * 4 is Pull
+	 */
+	public int getSelectedAbility(){
+		return selectedAbility;
 	}
 }
