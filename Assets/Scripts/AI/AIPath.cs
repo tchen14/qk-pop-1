3eca028ef72bcfced62edddcd25a6855b550469f
CAGD Student <cagd495@mailinator.com>
2015-10-13 16:50:53 -0700

    Path object works, it just needs to be more user friendly.

--- Assets/Scripts/AI/AIPath.cs
+++ Assets/Scripts/AI/AIPath.cs
@@ -4,16 +4,55 @@ using System.Collections.Generic;
 
 public class AIPath : MonoBehaviour {
 
-	public List<Checkpoint> checkpoints;
-
-	// Use this for initialization
-	void Start () {
+	public List<GameObject> checkpoints;
+	private GameObject instance = Resources.Load("checkpoint") as GameObject;
+	
+	void Start() {
 
 	}
 
-	void addCheckpoint()
+	public void addCheckpoint(GameObject new_point)
 	{
-		Checkpoint new_point = Instantiate(point);
-		checkpoints.Add(point);
+		Checkpoint c = new_point.GetComponent<Checkpoint>();
+		c.path_reference = this;
+		checkpoints.Add(new_point);
+	}
+
+	public void clearList(){
+		foreach (GameObject g in checkpoints) {
+			DestroyImmediate(g);
+		}
+		checkpoints.Clear();
+	}
+
+	public List<Vector3> getPoints(){
+		List<Vector3> points = new List<Vector3>();
+
+		foreach(GameObject c in checkpoints){
+
+			if(c == null){
+				checkpoints.Remove(c);
+			}
+			else{
+				Checkpoint data = c.GetComponent<Checkpoint>();
+				points.Add(data.getPosition());
+			}
+		}
+		return points;
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		List<Vector3> points = getPoints();
+		int length = points.Count;
+		Gizmos.color = Color.red;
+		
+		for (int i = 0; i < length; i++) {
+			if(i-1 >= 0){
+				Vector3 start = points[i-1];
+				Vector3 end = points[i];
+				Gizmos.DrawLine(start, end);
+			}
+		}
 	}
 }
