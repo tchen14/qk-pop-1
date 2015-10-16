947abeb784488f60108c4556cf117906a3bdd155
Forrest Spade <fspade@gmail.com>
2015-10-15 18:32:00 -0700

    Remove many things. Incorporate abilty chooser

---
+++ Assets/Scripts/Character/QK_AI_Character.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QK_AI_Character : MonoBehaviour {
+
+	NavMeshAgent agent;
+	QK_AI_Movement CharMove;
+
+	public Transform target;
+	float targetTolerance = 1;
+
+	Vector3 targetPos;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponentInChildren<NavMeshAgent> ();
+		CharMove = GetComponent<QK_AI_Movement> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (target != null) {
+			if ((target.position - targetPos).magnitude > targetTolerance) {
+				targetPos = target.position;
+				agent.SetDestination (targetPos);
+			}
+
+			agent.transform.position = transform.position;
+
+			CharMove.Move (agent.desiredVelocity);
+		} 
+		else {
+			CharMove.Move(Vector3.zero);
+		}
+	}
+}
