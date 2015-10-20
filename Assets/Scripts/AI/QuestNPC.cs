43eaa64b8677b804d7df5305e80463e726c80b99
CAGD Student <cagd495@mailinator.com>
2015-10-20 14:37:02 -0700

    updated

--- Assets/Scripts/AI/QuestNPC.cs
+++ Assets/Scripts/AI/QuestNPC.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[EventVisibleAttribute]
 public class QuestNPC : MonoBehaviour {
 
 	public Transform startLocation;
+	public int questID;
 
 
 	void Start () {
 		startLocation = this.gameObject.transform;
 	}
 
-	public void Reset() {
+	[EventVisibleAttribute]
+	public void ResetLocation() {
 		this.gameObject.transform.position = startLocation.position;
 		return;
 	}
