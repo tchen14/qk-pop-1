d0de2846db91bd31aea1073ca86f3d74d183ec41
JohnnyTurbo <jthompson59@mail.csuchico.edu>
2015-11-05 16:48:54 -0800

    Began work on the quest log UI. Quests will display on the screen and users can select them to view more details on each quest.
    Signed-off-by: JohnnyTurbo <jthompson59@mail.csuchico.edu>

---
+++ Assets/Scripts/UI/questProgressUpdater.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class questProgressUpdater : MonoBehaviour {
+
+	public GameObject progressPercentage;
+	public Slider progressSlider;
+
+	Text progressText;
+
+	void Start(){
+		if (progressPercentage == null) {
+			Debug.LogError("A quest progress slider does not have a UI text object to represent the " +
+			               "numerical value for progress.");
+		}
+		if (progressSlider == null) {
+			Debug.LogError("A quest progress slider does not have a UI slider object to reference for the " +
+			               "numerical value for progress.");
+		}
+		progressText = progressPercentage.gameObject.GetComponent<Text> ();
+		changeProgressPercentage ();
+	}
+
+	public void changeProgressPercentage(){
+		progressPercentage.GetComponent<Text>().text = progressSlider.value.ToString () + "%";
+	}
+}
